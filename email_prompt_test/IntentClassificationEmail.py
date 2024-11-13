4ddae25cfefda6cbb069c1012f5523544e2d2e8e
PrakashKunapareddy <saiprakash.kunapareddy@vassarlabs.com>
2024-11-13 18:16:13 +0530

    code refactoring

---
+++ email_prompt_test/IntentClassificationEmail.py
@@ -0,0 +1,156 @@
+from datetime import datetime
+import openpyxl
+import json
+import logging
+from langchain.chains import LLMChain
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate
+from langchain_community.chat_models import AzureChatOpenAI
+
+import SubIntentClassificationEmail
+from ExampleSearchChromaDB import fetch_similar_queries_for_intent, fetch_similar_queries
+from IntentClassificationPromptEmail import IntentClassificationPromptEmail
+from otherSubIntents import Others
+
+class NoHTTPRequestsFilter(logging.Filter):
+    def filter(self, record):
+        return 'HTTP' not in record.getMessage()
+
+
+def gpt_call(GPT):
+    gpt_config = {
+        "4omini": {
+            "OPENAI_API_KEY": "95058a9e99794e4689d179dd726e7eec",
+            "OPENAI_DEPLOYMENT_NAME": "vassar-4o-mini",
+            "OPENAI_EMBEDDING_MODEL_NAME": "vassar-text-ada002",
+            "OPENAI_API_BASE": "https://vassar-openai.openai.azure.com/",
+            "MODEL_NAME": "gpt-4o-mini",
+            "OPENAI_API_TYPE": "azure",
+            "OPENAI_API_VERSION": "2024-02-15-preview",
+        }
+    }
+    config = gpt_config.get(GPT)
+    if not config:
+        raise ValueError("Invalid GPT version specified")
+
+    llm = AzureChatOpenAI(
+        deployment_name=config["OPENAI_DEPLOYMENT_NAME"],
+        model_name=config["MODEL_NAME"],
+        openai_api_base=config["OPENAI_API_BASE"],
+        openai_api_type=config["OPENAI_API_TYPE"],
+        openai_api_key=config["OPENAI_API_KEY"],
+        openai_api_version=config["OPENAI_API_VERSION"],
+        temperature=0.0
+    )
+    return llm
+
+
+def intent_classification(email_body, examples, GPT):
+    llm = gpt_call(GPT)
+    prompt_template = ChatPromptTemplate(
+        messages=[
+            SystemMessagePromptTemplate.from_template(IntentClassificationPromptEmail.get("SYSTEM")),
+            HumanMessagePromptTemplate.from_template(IntentClassificationPromptEmail.get("CONTEXT")),
+            SystemMessagePromptTemplate.from_template(IntentClassificationPromptEmail.get("DISPLAY")),
+            SystemMessagePromptTemplate.from_template(IntentClassificationPromptEmail.get("REMEMBER"))
+        ]
+    )
+    llm_chain = LLMChain(llm=llm, prompt=prompt_template, verbose=False)
+    return llm_chain.run({"email_history": email_body, "examples": examples})
+
+
+def sub_intent_classification(email_body, intent, GPT):
+    llm = gpt_call(GPT)
+    sub_examples = fetch_similar_queries_for_intent(email_body, intent, top_k=10)
+    sub_intent_data = SubIntentClassificationEmail.sub_intents_email.get(intent.upper(), {})
+    if not sub_intent_data:
+        logging.warning(f"Sub-intent configuration not found for: {intent.upper()}")
+        return "No DATA FOR SUB-INTENT"
+
+    prompt_template = ChatPromptTemplate(
+        messages=[
+            SystemMessagePromptTemplate.from_template(sub_intent_data.get("SYSTEM")),
+            HumanMessagePromptTemplate.from_template(sub_intent_data.get("CONTEXT")),
+            SystemMessagePromptTemplate.from_template(sub_intent_data.get("DISPLAY")),
+            SystemMessagePromptTemplate.from_template(sub_intent_data.get("REMEMBER"))
+        ]
+    )
+    llm_chain = LLMChain(llm=llm, prompt=prompt_template, verbose=False)
+    result = llm_chain.run({"email_history": email_body, "examples": sub_examples})
+    return [result,sub_examples]
+
+
+import logging
+from datetime import datetime
+import json
+import openpyxl
+
+
+def process_email_classifications(file_path, GPT):
+    logging.basicConfig(
+        filename='email_classification.log',
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+    logger = logging.getLogger()
+
+    workbook = openpyxl.load_workbook(file_path)
+    sheet = workbook.active
+    formatted_time1 = datetime.now().strftime("%H:%M:%S")
+
+    for row in range(2, sheet.max_row + 1):
+        if sheet[f'A{row}'].value is None:
+            break
+
+        email_history = (sheet[f'C{row}'].value or "").split("USER_LATEST_EMAIL:")[0].strip()
+        user_latest_email = (sheet[f'C{row}'].value or "").split("USER_LATEST_EMAIL:")[1].strip() if sheet[
+            f'C{row}'].value else ""
+        user_latest_email_body = user_latest_email.split("Body:")[1] if "Body:" in user_latest_email else ""
+
+        examples = fetch_similar_queries(user_latest_email_body, top_k=10)
+        sub_examples = ""
+        classified_sub_intent = ""
+        email_body = f"{email_history}\n{user_latest_email}"
+        expected_intent = sheet[f'D{row}'].value.strip()
+
+        classified_intent = intent_classification(email_body, examples, GPT)
+        classified_data = json.loads(classified_intent)
+        intent = classified_data.get("intent", "")
+        final_intent = ""
+        if intent not in ["OTHERS", "BANTER"]:
+            classified_sub_intent_and_examples = sub_intent_classification(email_body, intent, GPT)
+            classified_sub_intent = classified_sub_intent_and_examples[0]
+            sub_examples = classified_sub_intent_and_examples[1]
+            sub_intent_data = json.loads(classified_sub_intent)
+            sub_intent = sub_intent_data.get("sub_intent").strip()
+
+            list_intents = [intent.strip() for intent in Others.get(intent, [])]
+            if sub_intent in list_intents:
+                final_intent = "OTHERS"
+            else:
+                final_intent = intent
+            sheet[f'E{row}'] = final_intent
+        else:
+            final_intent = intent
+            sheet[f'E{row}'] = final_intent
+
+        if expected_intent.lower().strip() == final_intent.lower().strip():
+            sheet[f'F{row}'] = "PASS"
+            logger.info(f"Row #{row}: PASS - Expected Intent: '{expected_intent}', Classified Intent: '{final_intent}'")
+        else:
+            sheet[f'F{row}'] = "FAIL"
+            logger.error(
+                f"Row #{row}: FAIL - Expected Intent: '{expected_intent}', Classified Intent: '{final_intent}'."
+                f"\nEmail History: {email_history}\nUser Latest Email: {user_latest_email_body}\n"
+                f"Intent Examples: {examples}\nSub Intent Data: {classified_sub_intent}\nSub Intent Examples: {sub_examples}"
+            )
+
+    workbook.save(file_path)
+    formatted_time2 = datetime.now().strftime("%H:%M:%S")
+    logger.info(f"Start Time: {formatted_time1}")
+    logger.info(f"End Time: {formatted_time2}")
+    logger.info(f"Updated Excel file saved at: {file_path}")
+
+
+
+file_path = '/home/saiprakesh/PycharmProjects/Prompt Testing Using Excel/email_prompt_test/automation_testing_email.xlsx'
+process_email_classifications(file_path, GPT="4omini")
---
+++ email_prompt_test/SubIntentClassificationEmail.py
@@ -0,0 +1,224 @@
+sub_intents_email = {
+    "PRODUCT_AVAILABILITY": {
+        "SYSTEM": """You are a sub intent identification bot for product availability. Based on the EMAIL_HISTORY, determine the Bot’s likely response and identify the sub intent of the bot's likely response for PRODUCT_AVAILABILITY intent.
+Look into SAMPLE INTENT IDENTIFICATION EXAMPLES for additional context.
+
+Classify the given email content into one of the following SUB_INTENTS:
+
+1. STOCK_STATUS:
+ - Only handles inquiries specifically asking about the availability or stock status of a specific product.
+
+2. ALTERNATIVE_PRODUCTS:
+ - Only handles inquiries asking for recommendations or suggestions for similar or alternative products when the desired product is out of stock or unavailable.
+
+3. PRODUCT_QUERIES:
+ - Only handles general queries about the product itself, such as dimensions, materials, how to use or assemble guide, features and specifications.
+
+4. DISCOUNTS_AND_PROMOTIONS:
+- Only handles inquiries about available discounts, sales, active coupons, promotions, or special offers related to the product or store.
+
+5. SHIPPING_TIMELINES:
+ - Only handles inquiries about the expected delivery time, estimated shipping dates, or specific timelines for when the product will be available.
+
+OTHERS:
+ - If the email content mentions multiple SUB_INTENTS or falls outside the scope of the above categories, classify it as "OTHERS."
+ - If the email content falls into one of the external INTENTS (DAMAGES, RETURNS, ORDER_STATUS, TRADE_APPLICATION), classify it as "OTHERS."
+""",
+
+        "CONTEXT": """
+EMAIL_HISTORY:
+{email_history}
+
+SAMPLE INTENT IDENTIFICATION EXAMPLES:
+{examples}
+""",
+        "DISPLAY": """Ensure that the output is in the following JSON format exactly as shown:
+{{
+  "sub_intent": "[Sub Intent Classified]",
+  "bot_likely_response": "[bot likely response]",
+  "last_message": "[last message]",
+  "reason": "[explain for the intent classified reason]"
+}}
+""",
+        "REMEMBER": """Prioritize the email body for intent classification. classify it accordingly based on that context. Return the intent of the bot's likely response. Follow each intent description.""",
+    },
+    "DAMAGES": {
+        "SYSTEM": """You are a sub intent identification bot for damages. Based on the EMAIL_HISTORY, determine the Bot’s likely response and identify the sub intent of the bot's likely response for DAMAGES intent.
+Look into SAMPLE INTENT IDENTIFICATION EXAMPLES for additional context.
+
+Classify the given email content into one of the following SUB_INTENTS:
+
+1. DAMAGED_DURING_SHIPPING:
+ - Only handles email reporting items that were damaged upon arrival or during shipping.
+
+2. DAMAGED_RETURN_REQUEST:
+ - Only handles email requesting to return an item because it was damaged during shipping.
+
+3. REPLACEMENT_REQUEST:
+ - Only handles email requesting a replacement for a damaged item during shipping.
+
+4. REFUSED_SHIPMENT:
+ - Only handles email stating that the shipment was refused by the recipient due to visible damage upon delivery.
+
+5. CLAIMS_AND_REFUND:
+ - Only handles email mentioning claims or compensation due to damage incurred during transit.
+
+6. DAMAGED_AFTER_DELIVERY:
+ - Only handles email reporting that the product became damaged or defective after successful delivery, typically during normal use.
+
+7. MISSING_ITEMS:
+- Only handles email reporting missing parts or items from the order, even if the shipment was received without visible damage.
+
+
+OTHERS:
+ - If the email content mentions multiple SUB_INTENTS or falls outside the scope of the above categories, classify it as "OTHERS."
+ - If the email content falls into one of the external INTENTS (PRODUCT_AVAILABILITY, RETURNS, ORDER_STATUS, TRADE_APPLICATION), classify it as "OTHERS."
+""",
+
+        "CONTEXT": """ 
+EMAIL_HISTORY:
+{email_history}
+
+SAMPLE INTENT IDENTIFICATION EXAMPLES:
+{examples}
+""",
+        "DISPLAY": """Ensure that the output is in the following JSON format exactly as shown:
+{{
+  "sub_intent": "[Sub Intent Classified]",
+  "bot_likely_response": "[bot likely response]",
+  "last_message": "[last message]",
+  "reason": "[explain for the intent classified reason]"
+}}
+""",
+        "REMEMBER": """Prioritize the email body for intent classification. classify it accordingly based on that context. Return the intent of the bot's likely response. Follow each intent description.""",
+    },
+    "ORDER_STATUS": {
+        "SYSTEM": """You are a sub intent identification bot for order status. Based on the EMAIL_HISTORY, determine the Bot’s likely response and identify the sub intent of the bot's likely response for order status intent.
+Look into SAMPLE INTENT IDENTIFICATION EXAMPLES for additional context.
+
+Classify the given email content into one of the following SUB_INTENTS:
+
+1. SHIPPING_UPDATES:
+ - Only Handles emails specifically inquiring about the status of an order that has already been placed. Concerning estimated shipping dates, tracking information.
+
+
+2. CANCELLATION:
+ - Only handles email requests to cancel an order.
+
+3. REFUND:
+ - Only handles email inquiries about the status of a refund related to an order.
+
+4. ORDER_CONFIRMATION:
+ - Only handles email inquiries about the confirmation of an order.
+
+5. ORDER_MODIFICATIONS:
+ - Only handles email requests to change details of an order, such as address or item changes.
+
+OTHERS:
+ - If the email content mentions multiple SUB_INTENTS or falls outside the scope of the above categories, classify it as "OTHERS."
+ - If the email content falls into one of the external INTENTS (PRODUCT_AVAILABILITY, DAMAGES, RETURNS, TRADE_APPLICATION), it should also be classified as "OTHERS".
+""",
+
+        "CONTEXT": """
+EMAIL_HISTORY:
+{email_history}
+SAMPLE INTENT IDENTIFICATION EXAMPLES:
+{examples}
+""",
+        "DISPLAY": """Ensure that the output is in the following JSON format exactly as shown:
+{{
+  "sub_intent": "[Sub Intent Classified]",
+  "bot_likely_response": "[bot likely response]",
+  "last_message": "[last message]",
+  "reason": "[explain for the intent classified reason]"
+}}
+""",
+        "REMEMBER": """Prioritize the email body for intent classification. classify it accordingly based on that context. Return the intent of the bot's likely response. Follow each intent description.""",
+    },
+    "RETURNS": {
+        "SYSTEM": """You are a sub intent identification bot for returns. Based on the EMAIL_HISTORY, determine the Bot’s likely response and identify the sub intent of the bot's likely response for returns.
+Look into SAMPLE INTENT IDENTIFICATION EXAMPLES for additional context.
+
+Classify the given email content into one of the following SUB_INTENTS:
+
+1. RETURNS_PROCESSING:
+ - Only handles emails that specifically mention a request for a return label, checking eligibility for returns, or initiating the return process.
+
+2. REFUND:
+ - Only handles if the email contains a request for a refund or inquiries about the status of a refund related to a return.
+
+3. REPLACEMENT:
+ - Only handles if the email asks for a replacement for an item.
+
+4. RETURN_POLICY:
+ - Only handles if the email asks about the terms, conditions, or details of the return policy like return period, conditions for return etc.
+
+5. QUERIES:
+ - Only handles if the email asks for general information or clarification on the return procedure without intending to return a product. Also includes cases where the user expresses dissatisfaction or unhappiness about the product without directly requesting a return.
+
+6. SHIPPING_CHARGES_ADJUSTMENT:
+ - Only handles emails requesting discounts on return shipping, special requests for free return labels, or any negotiations regarding the return shipping charges.
+
+OTHERS:
+ - If the email content mentions multiple SUB_INTENTS or falls outside the scope of the above categories, classify it as "OTHERS."
+ - If the email content falls into one of the external INTENTS (PRODUCT_AVAILABILITY, DAMAGES, ORDER_STATUS, TRADE_APPLICATION), it should also be classified as "OTHERS".
+""",
+        "CONTEXT": """
+EMAIL_HISTORY:
+{email_history}
+
+SAMPLE INTENT IDENTIFICATION EXAMPLES:
+{examples}
+
+""",
+        "DISPLAY": """Ensure that the output is in the following JSON format exactly as shown:
+{{
+  "sub_intent": "[Sub Intent Classified]",
+  "bot_likely_response": "[bot likely response]",
+  "last_message": "[last message]",
+  "reason": "[explain for the intent classified reason]"
+}}
+""",
+        "REMEMBER": """Prioritize the email body for intent classification. classify it accordingly based on that context. Return the intent of the bot’s likely response. Follow each intent description.""",
+    },
+    "TRADE_APPLICATION": {
+        "SYSTEM": """You are a sub intent identification bot for trade application. Based on the EMAIL_HISTORY, determine the Bot’s likely response and identify the sub intent of the bot's likely response for TRADE_APPLICATION intent.
+Look into SAMPLE INTENT IDENTIFICATION EXAMPLES for additional context.
+
+Classify the given email content into one of the following SUB_INTENTS:
+
+1. TRADE_ACCOUNT:
+ - Only handles inquiries and requests related to setting up a new trade account or updating details of an existing account.
+
+2. DISCOUNTS_AND_PRICING:
+ - Only handles inquiries about trade discounts, special pricing, or bulk pricing benefits available to trade account holders.
+
+3. PROGRAM_BENEFITS_QUERIES:
+ - Only handles questions regarding the benefits of the trade program, such as exclusive offers, access to special products, or other advantages of having a trade account.
+
+OTHERS:
+ - If the email content mentions multiple SUB_INTENTS or falls outside the scope of the above categories, classify it as "OTHERS."
+ - If the email content falls into one of the external INTENTS (DAMAGES, RETURNS, ORDER STATUS, PRODUCT AVAILABILITY), classify it as "OTHERS."
+""",
+
+        "CONTEXT": """
+EMAIL_HISTORY:
+{email_history}
+
+SAMPLE INTENT IDENTIFICATION EXAMPLES:
+{examples}
+""",
+        "DISPLAY": """Ensure that the output is in the following JSON format exactly as shown:
+{{
+  "sub_intent": "[Sub Intent Classified]",
+  "bot_likely_response": "[bot likely response]",
+  "last_message": "[last message]",
+  "reason": "[explain for the intent classified reason]"
+}}
+""",
+        "REMEMBER": """Prioritize the email body for intent classification. classify it accordingly based on that context. Return the intent of the bot’s likely response. Follow each intent description.""",
+    }
+}
+
+class SubIntentClassificationEmail:
+    pass
